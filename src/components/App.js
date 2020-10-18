93835db156ca2667dd0444d6df78a1258f8fed57
vip529 <srvip028@gmail.com>
2020-10-19 03:19:33 +0530

    change5

--- src/components/App.js
+++ src/components/App.js
@@ -29,48 +29,40 @@ const myData = {
     ]
   }
 
-class App extends Component {
-
-    jsonToJSX(data) {
-        let style = "";
-        if (Object.keys(data.style).length !== 0) {
-          let propArray = [];
-          for (let key in data.style) {
-            if (data.style.hasOwnProperty(key)) {
-              let nKey = key.split("-");
-              if(nKey.length > 1){
-                nKey[1] = nKey[1].charAt(0).toUpperCase() + nKey[1].slice(1)
-              }
-              nKey = nKey.join("")
-              propArray.push(`${nKey}:"${data.style[key]}"`);
+  export default function App(){
+    const jsonToJSX = (data) =>{
+      let style = "";
+      if (Object.keys(data.style).length !== 0) {
+        let propArray = [];
+        for (let key in data.style) {
+          if (data.style.hasOwnProperty(key)) {
+            let nKey = key.split("-");
+            if (nKey.length > 1) {
+              nKey[1] = nKey[1].charAt(0).toUpperCase() + nKey[1].slice(1);
             }
+            nKey = nKey.join("");
+            propArray.push(`${nKey}:"${data.style[key]}"`);
           }
-          let propString = propArray.join(", ");
-          style = `style={{${propString}}}`;
         }
-    
-        if (data.children.length === 0) {
-          return `
-          <${data.name} ${style}/>
-          `;
-        } else {
-          let childArray = (data.children || []).map((childData) => {
-            return this.jsonToJSX(childData);
-          });
-          let childString = childArray.join(" ");
-    
-          return `
-          <${data.name} ${style}>
+        let propString = propArray.join(", ");
+        style = `style={{${propString}}}`;
+      }
+  
+      if (data.children.length === 0) {
+        return `
+        <${data.name} ${style}/>
+        `;
+      } else {
+        let childArray = (data.children || []).map((childData) => {
+          return jsonToJSX(childData);
+        });
+        let childString = childArray.join(" ");
+  
+        return `<${data.name} ${style}>
               ${childString}
-          <${data.name}/>
-          `;
-        }
+            <${data.name}/>
+        `;
       }
-      render() {
-        let result = this.jsonToJSX(myData);
-        return JSON.stringify(result);;
-      }
-}
-
-
-export default App;
+    }
+    return jsonToJSX(myData);
+  }
