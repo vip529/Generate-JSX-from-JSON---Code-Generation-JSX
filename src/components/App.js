4b36188bfa4a6b87076e2e765be0b034dd9f372d
vip529 <srvip028@gmail.com>
2020-10-19 22:13:51 +0530

    c6

--- src/components/App.js
+++ src/components/App.js
@@ -1,40 +1,16 @@
-// import React, {Component, useState} from "react";
+import React, {Component, useState} from "react";
 import '../styles/App.css';
 
+class App extends Component {
+    render() {
 
-  export default function App(props){
-    const jsonToJSX = (data) =>{
-      let style = "";
-      if (Object.keys(data.style).length !== 0) {
-        let propArray = [];
-        for (let key in data.style) {
-          if (data.style.hasOwnProperty(key)) {
-            let nKey = key.split("-");
-            if (nKey.length > 1) {
-              nKey[1] = nKey[1].charAt(0).toUpperCase() + nKey[1].slice(1);
-            }
-            nKey = nKey.join("");
-            propArray.push(`${nKey}:${Number.isInteger(data.style[key])?`${data.style[key]}`:`"${data.style[key]}"`}`);
-          }
-        }
-        let propString = propArray.join(", ");
-        style = `style={{${propString}}}`;
-      }
-  
-      if (data.children.length === 0) {
-        return `
-        <${data.name} ${style}/>
-        `;
-      } else {
-        let childArray = (data.children || []).map((childData) => {
-          return jsonToJSX(childData);
-        });
-        let childString = childArray.join(" ");
-  
-        return `<${data.name} ${style}> 
-            ${childString} 
-          </${data.name}>`;
-      }
+        return(
+            <>
+               
+            </>
+        )
     }
-    return jsonToJSX(props).toString();
-  }
+}
+
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,8 +2,37 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 function generateCodeFromObject(obj){
-    //return a code generated string
-   }
+    const jsonToJSX = (data) =>{
+      let style = "";
+      if (Object.keys(data.style).length !== 0) {
+        let propArray = [];
+        for (let key in data.style) {
+          if (data.style.hasOwnProperty(key)) {
+            let nKey = key.split("-");
+            if (nKey.length > 1) {
+              nKey[1] = nKey[1].charAt(0).toUpperCase() + nKey[1].slice(1);
+            }
+            nKey = nKey.join("");
+            propArray.push(`${nKey}:${Number.isInteger(data.style[key])?`${data.style[key]}`:`"${data.style[key]}"`}`);
+          }
+        }
+        let propString = propArray.join(", ");
+        style = `style={{${propString}}}`;
+      }
+  
+      if (data.children.length === 0) {
+        return `<${data.name} ${style}/>`;
+      } else {
+        let childArray = (data.children || []).map((childData) => {
+          return jsonToJSX(childData);
+        });
+        let childString = childArray.join(" ");
+  
+        return `<${data.name} ${style}> ${childString} </${data.name}>`;
+      }
+    }
+    return jsonToJSX(obj).toString();    
+}
    
 module.exports=generateCodeFromObject;
 
